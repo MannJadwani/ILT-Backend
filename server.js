a651f25c63aa38c446e102ed23fd1d008f0ff1f2
Lakhan-bit <lakhankhobragade2003@gmail.com>
2026-06-29 12:16:46 +0530

    is active-2

--- server.js
+++ server.js
@@ -2736,19 +2736,21 @@ app.post('/issuepage_filterinputs_data', async (req, res) => {
         AND master_business_sector.description IS NOT NULL;
       `);
     const securityTypeOptions = await prisma.$queryRawUnsafe(`
-        SELECT DISTINCT master_security_type.description AS security_type
-        FROM master_issuer
-        LEFT JOIN master_security_type 
-          ON master_security_type.code = master_issuer.security_class
-        WHERE master_security_type.description IS NOT NULL AND is_active = 1;
-      `);
+      SELECT DISTINCT master_security_type.description AS security_type
+      FROM master_issuer
+      LEFT JOIN master_security_type 
+        ON master_security_type.code = master_issuer.security_class
+      WHERE master_security_type.description IS NOT NULL 
+        AND master_security_type.is_active = 1; 
+    `);
     const modeissueOptions = await prisma.$queryRawUnsafe(`
-        SELECT DISTINCT master_mode_issue.description AS mode_of_issue
-        FROM master_issuer
-        LEFT JOIN master_mode_issue
-          ON master_mode_issue.code = master_issuer.mode_issue
-        WHERE master_mode_issue.description IS NOT NULL AND is_active = 1;
-      `);
+      SELECT DISTINCT master_mode_issue.description AS mode_of_issue
+      FROM master_issuer
+      LEFT JOIN master_mode_issue
+        ON master_mode_issue.code = master_issuer.mode_issue
+      WHERE master_mode_issue.description IS NOT NULL 
+        AND master_mode_issue.is_active = 1;
+    `);
 
     const creditRatingOptions = await prisma.$queryRawUnsafe(`
         SELECT DISTINCT master_issuer_rating.rating AS credit_rating
